9fafdc5803d2148b19394306578af850913b7c12
claud <crobinson@tec.mx>
2024-04-12 11:55:41 -0600

    Initial Spring infrastructure.

---
+++ src/main/java/my/edu/mypetclinic2024/MyPetClinic2024Application.java
@@ -0,0 +1,13 @@
+package my.edu.mypetclinic2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetClinic2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPetClinic2024Application.class, args);
+	}
+
+}
