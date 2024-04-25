f8477194903afea2b107c855bf8a90a6b3361e8d
claud <crobinson@tec.mx>
2024-04-25 17:57:30 -0600

    Create JPA Owner Service. Closes #40

--- pet-clinic-data/src/main/java/my/edu/mypetclinic2024/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/my/edu/mypetclinic2024/repositories/OwnerRepository.java
@@ -4,5 +4,6 @@ import my.edu.mypetclinic2024.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 
 }
