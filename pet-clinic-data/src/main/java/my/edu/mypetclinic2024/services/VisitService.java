d837dc0822761731749369560e31a5e50ba32a2a
claud <crobinson@tec.mx>
2024-04-25 19:39:26 -0600

    Implemented MapService for Visit. Closes #46

---
+++ pet-clinic-data/src/main/java/my/edu/mypetclinic2024/services/VisitService.java
@@ -0,0 +1,6 @@
+package my.edu.mypetclinic2024.services;
+
+import my.edu.mypetclinic2024.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/my/edu/mypetclinic2024/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package my.edu.mypetclinic2024.services.map;
+
+import my.edu.mypetclinic2024.model.Visit;
+import my.edu.mypetclinic2024.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner() == null ) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
